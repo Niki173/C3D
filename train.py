ead771448f4b19dfc43960cd2b2f8b76cb0cb43d
Niki173 <78299831+Niki173@users.noreply.github.com>
2021-01-31 21:27:00 +0800

    Update train.py

--- train.py
+++ train.py
@@ -18,11 +18,11 @@ from network import C3D_model, R2Plus1D_model, R3D_model
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("Device being used:", device)
 
-nEpochs = 5  # Number of epochs for training
+nEpochs = 20  # Number of epochs for training
 resume_epoch = 0  # Default is 0, change if want to resume
 useTest = True # See evolution of the test set when training
-nTestInterval = 5 # Run on test set every nTestInterval epochs
-snapshot = 5 # Store a model every snapshot epochs
+nTestInterval = 10 # Run on test set every nTestInterval epochs
+snapshot = 10 # Store a model every snapshot epochs
 lr = 1e-3 # Learning rate
 
 dataset = 'ucf101' # Options: hmdb51 or ucf101
