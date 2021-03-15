55f2163c0acf1b6ef2fb84f9bbc6c29fa9d6acfd
Niki173 <78299831+Niki173@users.noreply.github.com>
2021-03-15 19:08:31 +0800

    Add files via upload

---
+++ mypath.py
@@ -0,0 +1,25 @@
+class Path(object):
+    @staticmethod
+    def db_dir(database):
+        if database == 'ucf101':
+            # folder that contains class labels
+            root_dir = 'data/UCF-101'
+
+            # Save preprocess data into output_dir
+            output_dir = 'data/ucf101'
+
+            return root_dir, output_dir
+        elif database == 'hmdb51':
+            # folder that contains class labels
+            root_dir = '/Path/to/hmdb-51'
+
+            output_dir = '/path/to/VAR/hmdb51'
+
+            return root_dir, output_dir
+        else:
+            print('Database {} not available.'.format(database))
+            raise NotImplementedError
+
+    @staticmethod
+    def model_dir():
+        return 'models/c3d-pretrained.pth'
